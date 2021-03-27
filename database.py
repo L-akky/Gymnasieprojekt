86fefca8aa2b8b2b68811458b29ee9eee2c543da
L-akky <ramstajohn@gmail.com>
2021-03-27 10:22:16 +0100

    yes

--- database.py
+++ database.py
@@ -1,13 +1,16 @@
 import sqlite3 
 from article import Article
+from sentiment_mock import Sentimenter_mock
+
 class Database:
     def __init__(self):
-        self.text = ""
+        self.articles = ""
 
     def send_to_base(self, a: "Article" ):
         conn = sqlite3.connect("Gyarte_db/gyatre.db")
         c = conn.cursor()
-        value_list = (a.source, a.title, a.url, a.keyword, a.text, "", "", "") #"" är temporärt tills jag har skapat fake senitimenter
+        s1 = Sentimenter_mock()
+        value_list = (a.source, a.title, a.url, a.keyword, a.text, s1.get_sentiment()[0], s1.get_sentiment()[1], s1.get_sentiment()[0])
         print("Inserting values")
         c.execute("INSERT INTO Articles VALUES(?,?,?,?,?,?,?,?)", value_list)
         c.execute("SELECT * FROM Articles")
@@ -16,12 +19,14 @@ class Database:
         conn.close()
         print("Commited and closed")
     
-    def get_text(self, subject, newspaper):
+    def get_articles(self, subject, newspaper):
         conn = sqlite3.connect("Gyarte_db/gyatre.db")
         c = conn.cursor()
-        print("Getting text")
-        c.execute("SELECT Source, Title, URL, Keyword, Text, Positive_Sentiment, Neutral_Sentiment, Negative_Sentiment FROM Articles WHERE Keyword=?" (subject,))
-        self.text = c.fetchall()
-        print("Got text")
+        print("Getting articles")
+        c.execute("SELECT Source, Title, URL, Keyword, Text, Positive_Sentiment, Neutral_Sentiment, Negative_Sentiment FROM Articles WHERE Keyword=?", (subject,))
+        print("executed")
+        self.article = c.fetchall()
+        print(self.article)
+        print("Got articles")
         conn.close()
         print("Closed")
--- sentiment_mock.py
+++ sentiment_mock.py
@@ -4,25 +4,30 @@ from database import Database
 
 class Sentimenter_mock:
     def __init__(self):
-        self.positive_sentiment = ""
-        self.neutral_sentiment = ""
-        self.negative_sentiment =""
+        self.positive_sentiment = 0.0
+        self.neutral_sentiment = 0.0
+        self.negative_sentiment = 0.0
 
 
     def sentiment_analysis(self, client, subject, newspaper):
             db = Database()
-            documents = [db.get_text(subject, newspaper)]
+            db.get_article(subject, newspaper)
+            n = 0
+            for articles in db.article:
+                documents = [db.article[n][4]]
+                n += 1
+            print("Crunching the numbers")
+            self.positive_sentiment = uniform(0.0,1.0)
+            self.neutral_sentiment = uniform(0.0,1.0)
+            self.negative_sentiment = uniform(0.0,1.0)
             print(documents)
             print("Overall scores: Positive={0:.2f}; Neutral={1:.2f}; Negative={2:.2f} \n".format(
-                positive_awnser,
-                neutral_awnser,
-                negative_awnser,
+                self.positive_sentiment,
+                self.neutral_sentiment,
+                self.negative_sentiment,
             ))
     
     def get_sentiment(self):
-        self.positive_sentiment = uniform(0.0,1.0)
-        self.neutral_sentiment = uniform(0.0,1.0)
-        self.negative_sentiment = uniform(0.0,1.0)
         return [self.positive_sentiment, self.neutral_sentiment, self.negative_sentiment]
 
 
